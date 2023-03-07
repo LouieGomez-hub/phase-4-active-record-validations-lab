34a21c54be78838b56799aee8aae806ef082a464
louie_gomez <kinggomez027.lg@gmail.com>
2023-03-07 17:29:50 -0500

    post custom validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,5 +3,11 @@ class Post < ApplicationRecord
     validates :content, length: { minimum: 250 }
     validates :summary, length: { maximum: 250 }
     validates :category, inclusion: { in: ['Fiction', 'Non-Fiction'] }
-    validate 
+    validate :title_is_clickbait
+
+    def title_is_clickbait
+        unless title.present? && title.include?("Won't Believe" || "Secret" || "Top [number]" || "Guess")
+            errors.add(:title, 'This title is not clickbait-y')
+        end
+    end
 end
